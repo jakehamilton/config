b32c4b62bac274fcddce25de56ba09164a301d65
Jake Hamilton <jake.hamilton@hey.com>
2022-01-16 03:09:40 -0800

    feat: override vscode to support wayland

--- modules/apps/vscode.nix
+++ modules/apps/vscode.nix
@@ -10,6 +10,19 @@ in
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ vscode ];
+    environment.systemPackages = with pkgs; [
+      # @NOTE(jakehamilton): Code doesn't render properly
+      # on Wayland by default. We need to pass two params
+      # to make it render correctly.
+      (vscode.overrideAttrs (oldAttrs: {
+        buildInputs = oldAttrs.buildInputs or [] ++ [ pkgs.makeWrapper ];
+
+        postInstall = oldAttrs.postInstall or "" + ''
+          wrapProgram $out/bin/code \
+            --add-flags "--enable-features=UseOzonePlatform" \
+            --add-flags "--ozone-platform=wayland"
+        '';
+      }))
+    ];
   };
 }
