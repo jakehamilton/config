7d8cdd7d9311de0bf134c3c2aeb5c6c6c2e2cbcd
Jake Hamilton <jake.hamilton@hey.com>
2022-01-17 15:51:27 -0800

    refactor: remove commented code

--- lib/overlay.nix
+++ lib/overlay.nix
@@ -1,11 +1,6 @@
 inputs@{ lib, ... }:
 
 rec {
-  # mkOverlays = names: channels:
-  #   lib.map
-  #   (name: import (lib.getOverlayPath name) (inputs // { inherit channels; }))
-  #   names;
-
   mkOverlays = { src ? ../overlays }: channels:
     let
       overlays = lib.getModuleFilesWithoutDefaultRec src;
