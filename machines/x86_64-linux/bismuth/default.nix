93aa188a49760bdf960d5a3488df8938a85bd807
Jake Hamilton <jake.hamilton@hey.com>
2022-03-19 21:07:24 -0700

    refactor: update audio module and looking-glass config

--- machines/x86_64-linux/bismuth/default.nix
+++ machines/x86_64-linux/bismuth/default.nix
@@ -110,7 +110,64 @@ with lib; {
     };
 
     hardware = {
-      audio = enabled;
+      audio = {
+        enable = true;
+
+        alsa-monitor.rules = [
+          (mkAlsaRename {
+            name = "alsa_output.pci-0000_31_00.4.analog-stereo";
+            description = "Speakers";
+          })
+          (mkAlsaRename {
+            name =
+              "alsa_input.usb-Valve_Corporation_Valve_VR_Radio___HMD_Mic_426C59CC3D-LYM-01.mono-fallback";
+            description = "Valve Index";
+          })
+          (mkAlsaRename {
+            name =
+              "alsa_output.usb-Blue_Microphones_Yeti_Stereo_Microphone_797_2020_06_11_32800-00.analog-stereo";
+            description = "Blue Yeti";
+          })
+        ];
+
+        nodes = [
+          (mkVirtualAudioNode { name = "Desktop"; })
+          (mkVirtualAudioNode { name = "Discord"; })
+          (mkVirtualAudioNode {
+            name = "Headphones";
+            class = "Audio/Sink";
+          })
+          (mkVirtualAudioNode {
+            name = "Speakers";
+            class = "Audio/Sink";
+          })
+        ];
+
+        modules = [
+          (mkBridgeAudioModule {
+            name = "speakers";
+            from = "virtual-speakers-audio";
+            to = "alsa_output.pci-0000_31_00.4.analog-stereo";
+          })
+          (mkBridgeAudioModule {
+            name = "headphones";
+            from = "virtual-headphones-audio";
+            to =
+              "alsa_output.usb-Blue_Microphones_Yeti_Stereo_Microphone_797_2020_06_11_32800-00.analog-stereo";
+          })
+          (mkBridgeAudioModule {
+            name = "speakers-to-desktop";
+            from = "virtual-speakers-audio";
+            to = "virtual-desktop-audio";
+          })
+          (mkBridgeAudioModule {
+            name = "headphones-to-desktop";
+            from = "virtual-headphones-audio";
+            to = "virtual-desktop-audio";
+          })
+        ];
+      };
+
       networking = enabled;
       storage = enabled;
     };
--- modules/hardware/audio.nix
+++ modules/hardware/audio.nix
@@ -5,6 +5,11 @@ let cfg = config.plusultra.hardware.audio;
 in {
   options.plusultra.hardware.audio = with types; {
     enable = mkBoolOpt false "Whether or not to enable audio support.";
+    alsa-monitor = mkOpt attrs { } "Alsa configuration.";
+    nodes = mkOpt (listOf attrs) [ ]
+      "Audio nodes to pass to Pipewire as `context.objects`.";
+    modules = mkOpt (listOf attrs) [ ]
+      "Audio modules to pass to Pipewire as `context.modules`.";
   };
 
   config = mkIf cfg.enable {
@@ -15,87 +20,11 @@ in {
       pulse.enable = true;
       jack.enable = true;
 
-      media-session.config.alsa-monitor = {
-        rules = [
-          {
-            matches =
-              [{ "node.name" = "alsa_output.pci-0000_31_00.4.analog-stereo"; }];
-            actions = {
-              "update-props" = { "node.description" = "Speakers"; };
-            };
-          }
-          {
-            matches = [{
-              "node.name" =
-                "alsa_input.usb-Valve_Corporation_Valve_VR_Radio___HMD_Mic_426C59CC3D-LYM-01.mono-fallback";
-            }];
-            actions = {
-              "update-props" = { "node.description" = "Valve Index"; };
-            };
-          }
-          {
-            matches = [{
-              "node.name" =
-                "alsa_output.usb-Blue_Microphones_Yeti_Stereo_Microphone_797_2020_06_11_32800-00.analog-stereo";
-            }];
-            actions = {
-              "update-props" = { "node.description" = "Blue Yeti"; };
-            };
-          }
-        ];
-      };
+      media-session.config.alsa-monitor =
+        mkAliasDefinitions options.plusultra.hardware.audio.alsa-monitor;
 
       config.pipewire = {
-        "context.objects" = [
-          {
-            factory = "adapter";
-            args = {
-              "factory.name" = "support.null-audio-sink";
-              "node.name" = "virtual-desktop-audio";
-              "node.description" = "Desktop Audio (Virtual)";
-              "media.class" = "Audio/Duplex";
-              "object.linger" = true;
-              "audio.position" = [ "FL" "FR" ];
-              "monitor.channel-volumes" = true;
-            };
-          }
-          {
-            factory = "adapter";
-            args = {
-              "factory.name" = "support.null-audio-sink";
-              "node.name" = "virtual-discord-audio";
-              "node.description" = "Discord Audio (Virtual)";
-              "media.class" = "Audio/Duplex";
-              "object.linger" = true;
-              "audio.position" = [ "FL" "FR" ];
-              "monitor.channel-volumes" = true;
-            };
-          }
-          {
-            factory = "adapter";
-            args = {
-              "factory.name" = "support.null-audio-sink";
-              "node.name" = "virtual-headphones-audio";
-              "node.description" = "Headphones (Virtual)";
-              "media.class" = "Audio/Sink";
-              "object.linger" = true;
-              "audio.position" = [ "FL" "FR" ];
-              "monitor.channel-volumes" = true;
-            };
-          }
-          {
-            factory = "adapter";
-            args = {
-              "factory.name" = "support.null-audio-sink";
-              "node.name" = "virtual-speakers-audio";
-              "node.description" = "Speakers (Virtual)";
-              "media.class" = "Audio/Sink";
-              "object.linger" = true;
-              "audio.position" = [ "FL" "FR" ];
-              "monitor.channel-volumes" = true;
-            };
-          }
-        ];
+        "context.objects" = cfg.nodes ++ [ ];
         "context.modules" = [
           {
             name = "libpipewire-module-rtkit";
@@ -120,85 +49,7 @@ in {
           { name = "libpipewire-module-adapter"; }
           { name = "libpipewire-module-link-factory"; }
           { name = "libpipewire-module-session-manager"; }
-          {
-            name = "libpipewire-module-loopback";
-            args = {
-              "node.name" = "speakers-bridge";
-              "node.passive" = false;
-              "audio.position" = [ "FL" "FR" ];
-              "monitor.channel-volumes" = true;
-              "capture.props" = { "node.target" = "virtual-speakers-audio"; };
-              "playback.props" = {
-                "monitor.channel-volumes" = true;
-                "node.target" = "alsa_output.pci-0000_31_00.4.analog-stereo";
-              };
-            };
-          }
-          {
-            name = "libpipewire-module-loopback";
-            args = {
-              "node.name" = "headphones-bridge";
-              "audio.position" = [ "FL" "FR" ];
-
-              "monitor.channel-volumes" = true;
-              "capture.props" = { "node.target" = "virtual-headphones-audio"; };
-              "playback.props" = {
-                "monitor.channel-volumes" = true;
-                "node.target" =
-                  "alsa_output.usb-Blue_Microphones_Yeti_Stereo_Microphone_797_2020_06_11_32800-00.analog-stereo";
-              };
-            };
-          }
-          {
-            name = "libpipewire-module-loopback";
-            args = {
-              "node.name" = "desktop-audio-bridge (Speakers)";
-              "audio.position" = [ "FL" "FR" ];
-              "monitor.channel-volumes" = true;
-              "capture.props" = { "node.target" = "virtual-speakers-audio"; };
-              "playback.props" = {
-                "node.target" = "virtual-desktop-audio";
-                "monitor.channel-volumes" = true;
-              };
-            };
-          }
-          {
-            name = "libpipewire-module-loopback";
-            args = {
-              "node.name" = "desktop-audio-bridge (Headphones)";
-              "audio.position" = [ "FL" "FR" ];
-              "stream.props" = { "volume" = 0.5; };
-              "capture.props" = { "node.target" = "virtual-headphones-audio"; };
-              "playback.props" = {
-                "node.target" = "virtual-desktop-audio";
-                "monitor.channel-volumes" = true;
-              };
-            };
-          }
-          {
-            name = "libpipewire-module-loopback";
-            args = {
-              "node.name" = "default-bridge";
-              "audio.position" = [ "FL" "FR" ];
-              "monitor.channel-volumes" = true;
-              "capture.props" = {
-                # Disables the capture
-                "media.class" = "Audio/Source";
-                # This doesn't work, instead it selects the monitor for the default *source*:
-                # "node.target" = "@DEFAULT_SINK@";
-
-                # This does work, but will only ever be this named device:
-                # "node.target" = "alsa_output.pci-0000_31_00.4.analog-stereo";
-              };
-              "playback.props" = {
-                "monitor.channel-volumes" = true;
-                "node.target" = "virtual-desktop-audio";
-                # "node.target" = "@DEFAULT_SINK@";
-                # "audio.position" = [ "FL" "FR" ];
-              };
-            };
-          }
-        ];
+        ] ++ cfg.modules;
       };
     };
 
