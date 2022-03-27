41e962f958ee60b3eb9a1e79df0de61ca8deb09d
Jake Hamilton <jake.hamilton@hey.com>
2022-03-27 04:35:03 -0700

    feat: add more functionality to revision script

--- packages/nixos-revision/default.nix
+++ packages/nixos-revision/default.nix
@@ -1,5 +1,57 @@
 { pkgs, lib, ... }:
 
 pkgs.writeShellScriptBin "nixos-revision" ''
-  echo $(nixos-version --json | ${pkgs.jq}/bin/jq -r .configurationRevision)
+  HAS_HELP=false
+  HAS_OPEN=false
+
+  while [[ $# -gt 0 ]]; do
+  	case $1 in
+  		-h|--help)
+  			HAS_HELP=true
+  			shift
+  			;;
+  		-o|--open)
+  			HAS_OPEN=true
+  			shift
+  			;;
+  		*)
+  			shift
+  			;;
+  	esac
+  done
+
+  if [ $HAS_HELP == true ]; then
+  	HELP_MSG="
+  nixos-revision
+
+  USAGE
+
+    nixos-revision [options]
+
+  OPTIONS
+
+    -h, --help              Show this help message
+    -o, --open              Open the revision on GitHub
+
+  EXAMPLES
+
+    $ # Print the current revision
+    $ nixos-revision
+
+    $ # Open the current revision on GitHub
+    $ nixos-revision --open
+  "
+  	echo "$HELP_MSG"
+    exit 0
+  fi
+
+  REVISION=$(nixos-version --json | ${pkgs.jq}/bin/jq -r .configurationRevision)
+
+  if [ $HAS_OPEN == true ]; then
+    GITHUB_URL="https://github.com/jakehamilton/config/commit/$REVISION"
+    echo "Opening URL: $GITHUB_URL"
+    ${pkgs.xdg-utils}/bin/xdg-open $GITHUB_URL
+  else
+    echo $REVISION
+  fi
 ''
