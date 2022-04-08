df566a7cd7639d7dd37455136243f20cb01ceab7
Jake Hamilton <jake.hamilton@hey.com>
2022-04-07 20:58:11 -0700

    feat: add isolated environment for configuring yubikeys

---
+++ machines/x86_64-iso/isolated/default.nix
@@ -0,0 +1,106 @@
+{ pkgs, lib, ... }:
+
+with lib;
+let
+  gpgConf = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/drduh/config/master/gpg.conf";
+    sha256 = "0va62sgnah8rjgp4m6zygs4z9gbpmqvq9m3x4byywk1dha6nvvaj";
+  };
+  guide = pkgs.fetchurl {
+    url =
+      "https://raw.githubusercontent.com/drduh/YubiKey-Guide/master/README.md";
+    sha256 = "164pyqm3yjybxlvwxzfb9mpp38zs9rb2fycngr6jv20n3vr1dipj";
+  };
+  theme = pkgs.fetchFromGitHub {
+    owner = "jez";
+    repo = "pandoc-markdown-css-theme";
+    rev = "019a4829242937761949274916022e9861ed0627";
+    sha256 = "1h48yqffpaz437f3c9hfryf23r95rr319lrb3y79kxpxbc9hihxb";
+  };
+  guideHTML = pkgs.runCommand "yubikey-guide" { } ''
+    ${pkgs.pandoc}/bin/pandoc \
+      --standalone \
+      --metadata title="Yubikey Guide" \
+      --from markdown \
+      --to html5+smart \
+      --toc \
+      --template ${theme}/template.html5 \
+      --css ${theme}/docs/css/theme.css \
+      --css ${theme}/docs/css/skylighting-solarized-theme.css \
+      -o $out \
+      ${guide}
+  '';
+in {
+  services.pcscd.enable = true;
+  services.udev.packages = with pkgs; [ yubikey-personalization ];
+
+  environment.systemPackages = with pkgs; [
+    gnupg
+    pinentry-curses
+    pinentry-qt
+    paperkey
+    wget
+  ];
+
+  programs = {
+    ssh.startAgent = false;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+  };
+
+  plusultra = {
+    nix = enabled;
+
+    desktop = {
+      gnome = {
+        enable = true;
+        wallpaper = pkgs.plusultra.wallpapers.atmosphere.fileName;
+      };
+    };
+
+    apps = {
+      vscode = enabled;
+      firefox = enabled;
+    };
+
+    cli-apps = {
+      neovim = enabled;
+      yubikey = enabled;
+    };
+
+    tools = {
+      misc = enabled;
+      git = enabled;
+    };
+
+    home.file."gpg.conf".source = gpgConf;
+    home.file."guide.md".source = guide;
+    home.file."guide.html".source = guideHTML;
+
+    hardware = {
+      networking = {
+        # Networking is explicitly disabled in this environment.
+        enable = mkForce false;
+      };
+    };
+
+    security = { doas = enabled; };
+
+    system = {
+      fonts = enabled;
+      locale = enabled;
+      time = enabled;
+      xkb = enabled;
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+}
