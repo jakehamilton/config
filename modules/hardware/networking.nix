6887ed43afe99af99a58a3fa4e4907cb915d9593
Jake Hamilton <jake.hamilton@hey.com>
2022-04-13 20:33:52 -0700

    feat: add networking hosts and customization option

--- modules/hardware/networking.nix
+++ modules/hardware/networking.nix
@@ -4,17 +4,26 @@ with lib;
 let cfg = config.plusultra.hardware.networking;
 in {
   options.plusultra.hardware.networking = with types; {
-    enable = mkBoolOpt false "Whether or not to enable networking support.";
+    enable = mkBoolOpt false "Whether or not to enable networking support";
+    hosts = mkOpt attrs { }
+      "An attribute set to merge with <option>networking.hosts</option>";
   };
 
   config = mkIf cfg.enable {
-    networking.networkmanager.enable = true;
 
     plusultra.user.extraGroups = [ "networkmanager" ];
 
-    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-    # Per-interface useDHCP will be mandatory in the future, so this generated config
-    # replicates the default behaviour.
-    networking.useDHCP = false;
+    networking = {
+      hosts = {
+        "127.0.0.1" = [ "local.test" ] ++ (cfg.hosts."127.0.0.1" or [ ]);
+      } // cfg.hosts;
+
+      # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+      # Per-interface useDHCP will be mandatory in the future, so this generated config
+      # replicates the default behaviour.
+      useDHCP = false;
+
+      networkmanager = enabled;
+    };
   };
 }
