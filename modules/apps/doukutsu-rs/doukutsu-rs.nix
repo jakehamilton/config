5086df89506ab2e3fb0087103cab9523ef291aec
Jake Hamilton <jake.hamilton@hey.com>
2022-06-29 19:51:41 -0700

    feat: add module and desktop entry for doukutsu-rs

---
+++ modules/apps/doukutsu-rs/doukutsu-rs.nix
@@ -0,0 +1,28 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.plusultra.apps.doukutsu-rs;
+  desktopItem = pkgs.makeDesktopItem {
+    name = "doukutsu-rs";
+    desktopName = "doukutsu-rs";
+    genericName =
+      "A fully playable re-implementation of Cave Story (Doukutsu Monogatari) engine written in Rust.";
+    exec = "${pkgs.plusultra.doukutsu-rs}/bin/doukutsu-rs";
+    icon = ./icon.png;
+    type = "Application";
+    categories = [ "Game" "AdventureGame" ];
+    terminal = false;
+  };
+in {
+  options.plusultra.apps.doukutsu-rs = with types; {
+    enable = mkBoolOpt false "Whether or not to enable doukutsu-rs.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs.plusultra; [
+      doukutsu-rs
+      desktopItem
+    ];
+  };
+}
