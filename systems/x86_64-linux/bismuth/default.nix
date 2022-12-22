2edf5d35969bbe0db41eb8816cb61300c3a85139
Jake Hamilton <jake.hamilton@hey.com>
2022-12-21 19:28:37 -0800

    feat(bismuth): enable avahi

--- systems/x86_64-linux/bismuth/default.nix
+++ systems/x86_64-linux/bismuth/default.nix
@@ -24,6 +24,33 @@ with lib;
       plusultra.kalidoface
     ];
 
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+
+    extraServiceFiles = {
+      smb = ''
+        <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+        <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+        <service-group>
+          <name replace-wildcards="yes">%h</name>
+          <service>
+            <type>_smb._tcp</type>
+            <port>445</port>
+          </service>
+        </service-group>
+      '';
+    };
+  };
+
   services.samba = {
     enable = true;
     openFirewall = true;
