62a91ecb05617f5776ccc7b337fcb34f859b88e8
Jake Hamilton <jake.hamilton@hey.com>
2023-01-04 18:07:30 -0800

    fix(firefox): disable browser nags

--- modules/apps/firefox/default.nix
+++ modules/apps/firefox/default.nix
@@ -4,6 +4,8 @@ with lib;
 let
   cfg = config.plusultra.apps.firefox;
   defaultSettings = {
+    "browser.aboutwelcome.enabled" = false;
+    "browser.meta_refresh_when_inactive.disabled" = true;
     "browser.startup.homepage" =
       "https://start.duckduckgo.com/?kak=-1&kal=-1&kao=-1&kaq=-1&kt=Hack+Nerd+Font&kae=d&ks=m&k7=2e3440&kj=3b4252&k9=eceff4&kaa=d8dee9&ku=1&k8=d8dee9&kx=81a1c1&k21=3b4252&k18=1&k5=2&kp=-2&k1=-1&kaj=u&kay=b&kk=-1&kax=-1&kap=-1&kau=-1";
     "browser.bookmarks.showMobileBookmarks" = true;
