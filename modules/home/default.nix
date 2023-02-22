3fe55ab634fa500955b1f6e79afc39f89f4826b3
Jake Hamilton <jake.hamilton@hey.com>
2023-02-21 19:47:13 -0800

    chore: update snowfall-lib

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,10 +1,14 @@
-inputs@{ options, config, pkgs, lib, home-manager, ... }:
+{ options, config, pkgs, lib, inputs, ... }:
 
 with lib;
 with lib.internal;
 let cfg = config.plusultra.home;
 in
 {
+  imports = with inputs; [
+    home-manager.nixosModules.home-manager
+  ];
+
   options.plusultra.home = with types; {
     file = mkOpt attrs { }
       "A set of files to be managed by home-manager's <option>home.file</option>.";
