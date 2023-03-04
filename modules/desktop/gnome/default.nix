d4f62f8245ea72483c974a156f3e49125c9d4d9e
Jake Hamilton <jake.hamilton@hey.com>
2023-03-04 01:43:30 -0800

    fix: add keybindings for switching and moving windows to workspaces

--- modules/desktop/gnome/default.nix
+++ modules/desktop/gnome/default.nix
@@ -176,16 +176,27 @@ in
             num-workspaces = 10;
           };
           "org/gnome/desktop/wm/keybindings" = {
-            move-to-workspace-1 = [ "<Super>1" ];
-            move-to-workspace-2 = [ "<Super>2" ];
-            move-to-workspace-3 = [ "<Super>3" ];
-            move-to-workspace-4 = [ "<Super>4" ];
-            move-to-workspace-5 = [ "<Super>5" ];
-            move-to-workspace-6 = [ "<Super>6" ];
-            move-to-workspace-7 = [ "<Super>7" ];
-            move-to-workspace-8 = [ "<Super>8" ];
-            move-to-workspace-9 = [ "<Super>9" ];
-            move-to-workspace-10 = [ "<Super>0" ];
+            switch-to-workspace-1 = [ "<Super>1" ];
+            switch-to-workspace-2 = [ "<Super>2" ];
+            switch-to-workspace-3 = [ "<Super>3" ];
+            switch-to-workspace-4 = [ "<Super>4" ];
+            switch-to-workspace-5 = [ "<Super>5" ];
+            switch-to-workspace-6 = [ "<Super>6" ];
+            switch-to-workspace-7 = [ "<Super>7" ];
+            switch-to-workspace-8 = [ "<Super>8" ];
+            switch-to-workspace-9 = [ "<Super>9" ];
+            switch-to-workspace-10 = [ "<Super>0" ];
+
+            move-to-workspace-1 = [ "<Shift><Super>1" ];
+            move-to-workspace-2 = [ "<Shift><Super>2" ];
+            move-to-workspace-3 = [ "<Shift><Super>3" ];
+            move-to-workspace-4 = [ "<Shift><Super>4" ];
+            move-to-workspace-5 = [ "<Shift><Super>5" ];
+            move-to-workspace-6 = [ "<Shift><Super>6" ];
+            move-to-workspace-7 = [ "<Shift><Super>7" ];
+            move-to-workspace-8 = [ "<Shift><Super>8" ];
+            move-to-workspace-9 = [ "<Shift><Super>9" ];
+            move-to-workspace-10 = [ "<Shift><Super>0" ];
           };
           "org/gnome/mutter" = {
             edge-tiling = false;
@@ -261,6 +272,10 @@ in
               "a11y"
               "quickSettings"
             ];
+          };
+
+          "org/gnome/shell/extensions/space-bar/shortcuts" = {
+            enable-activate-workspace-shortcuts = false;
           };
         };
     };
