8e642909ed471a926a5d7c1e4c59b7dcb2a38c2f
Jake Hamilton <jake.hamilton@hey.com>
2023-04-03 02:38:03 -0700

    fix: reduce number of boot entries

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -11,7 +11,7 @@ in
 
   config = mkIf cfg.enable {
     boot.loader.systemd-boot.enable = true;
-    boot.loader.systemd-boot.configurationLimit = 10;
+    boot.loader.systemd-boot.configurationLimit = 5;
     boot.loader.efi.canTouchEfiVariables = true;
 
     # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
