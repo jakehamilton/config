fd53b8ed0a2efab9ce0f2eef2335a12f5fb2d25c
Jake Hamilton <jake.hamilton@hey.com>
2023-10-30 13:54:21 -0700

    fix: use custom tmux on nixos

--- modules/home/cli-apps/tmux/default.nix
+++ modules/home/cli-apps/tmux/default.nix
@@ -1,11 +1,13 @@
-{ lib, config, pkgs, ... }:
-
-with lib;
-with lib.plusultra;
-let
-  cfg = config.plusultra.cli-apps.tmux;
-in
 {
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.plusultra; let
+  cfg = config.plusultra.cli-apps.tmux;
+in {
   options.plusultra.cli-apps.tmux = {
     enable = mkEnableOption "Tmux";
   };
--- modules/nixos/cli-apps/tmux/default.nix
+++ modules/nixos/cli-apps/tmux/default.nix
@@ -1,67 +1,20 @@
-{ options, config, lib, pkgs, ... }:
-
-with lib;
-with lib.plusultra;
-let
-  cfg = config.plusultra.cli-apps.tmux;
-  configFiles = lib.snowfall.fs.get-files ./config;
-
-  # Extrakto with wl-clipboard patched in.
-  extrakto = pkgs.tmuxPlugins.mkTmuxPlugin {
-    pluginName = "extrakto";
-    version = "unstable-2021-04-04-wayland";
-    src = pkgs.fetchFromGitHub {
-      owner = "laktak";
-      repo = "extrakto";
-      rev = "de8ac3e8a9fa887382649784ed8cae81f5757f77";
-      sha256 = "0mkp9r6mipdm7408w7ls1vfn6i3hj19nmir2bvfcp12b69zlzc47";
-    };
-    nativeBuildInputs = [ pkgs.makeWrapper ];
-    postInstall = ''
-      for f in extrakto.sh open.sh tmux-extrakto.sh; do
-        wrapProgram $target/scripts/$f \
-          --prefix PATH : ${with pkgs; lib.makeBinPath (
-          [ pkgs.fzf pkgs.python3 pkgs.xclip wl-clipboard ]
-          )}
-      done
-    '';
-    meta = {
-      homepage = "https://github.com/laktak/extrakto";
-      description = "Fuzzy find your text with fzf instead of selecting it by hand ";
-      license = lib.licenses.mit;
-      platforms = lib.platforms.unix;
-    };
-  };
-
-  plugins =
-    [ extrakto ] ++
-    (with pkgs.tmuxPlugins; [
-      continuum
-      nord
-      tilish
-      tmux-fzf
-      vim-tmux-navigator
-    ]);
-in
 {
-  options.plusultra.cli-apps.tmux = with types; {
-    enable = mkBoolOpt false "Whether or not to enable tmux.";
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.plusultra; let
+  cfg = config.plusultra.cli-apps.tmux;
+in {
+  options.plusultra.cli-apps.tmux = {
+    enable = mkEnableOption "Tmux";
   };
 
   config = mkIf cfg.enable {
-    plusultra.home.extraOptions = {
-      programs.tmux = {
-        enable = true;
-        terminal = "screen-256color-bce";
-        clock24 = true;
-        historyLimit = 2000;
-        keyMode = "vi";
-        newSession = true;
-        extraConfig = builtins.concatStringsSep "\n"
-          (builtins.map lib.strings.fileContents configFiles);
-
-        inherit plugins;
-      };
-    };
+    environment.systemPackages = with pkgs; [
+      plusultra.tmux
+    ];
   };
 }
