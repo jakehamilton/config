8b2aed2ddaa55565f9ec895f768bb921ec4f7115
Jake Hamilton <jake.hamilton@hey.com>
2024-01-19 19:35:14 -0800

    fix: remove attic and hydra from ruby

--- systems/x86_64-linux/ruby/default.nix
+++ systems/x86_64-linux/ruby/default.nix
@@ -1,23 +1,15 @@
-{ lib, pkgs, config, ... }:
-
-with lib;
-with lib.plusultra;
-let
-  build-machine = hostName: system: speedFactor: {
-    inherit hostName system speedFactor;
-    sshUser = "short";
-  };
-in
 {
-  imports = [ ./hardware.nix ];
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+with lib;
+with lib.plusultra; {
+  imports = [./hardware.nix];
 
   plusultra = {
     nix = enabled;
-
-    cache.public = {
-      # Ruby runs the cache, it can just use its own store.
-      enable = mkForce false;
-    };
 
     cli-apps = {
       neovim = enabled;
@@ -38,111 +30,14 @@ in
     };
 
     services = {
-      openssh = {
-        enable = true;
-
-        # Hydra needs to SSH to other machines and explodes if we remap
-        # to another port.
-        manage-other-hosts = false;
-      };
+      openssh = enabled;
       tailscale = enabled;
-
-      attic = {
-        enable = true;
-
-        user = "atticd";
-        group = "atticd";
-
-        settings = {
-          listen = "[::]:8989";
-
-          chunking = {
-            nar-size-threshold = 64 * 1024;
-            min-size = 16 * 1024;
-            avg-size = 64 * 1024;
-            max-size = 256 * 1024;
-          };
-
-          require-proof-of-possession = false;
-        };
-      };
-
-      vault-agent = {
-        enable = true;
-
-        settings = {
-          vault.address = "https://vault.quartz.hamho.me";
-        };
-
-        services = {
-          atticd = {
-            settings = {
-              vault.address = "https://vault.quartz.hamho.me";
-
-              auto_auth = {
-                method = [{
-                  type = "approle";
-
-                  config = {
-                    role_id_file_path = "/var/lib/vault-agent/attic/role-id";
-                    secret_id_file_path = "/var/lib/vault-agent/attic/secret-id";
-
-                    remove_secret_id_file_after_reading = false;
-                  };
-                }];
-              };
-            };
-
-            secrets.environment.templates = {
-              atticd-env = {
-                text = ''
-                  {{ with secret "secret/service/attic/atticd" }}
-                  ATTIC_SERVER_TOKEN_HS256_SECRET_BASE64="{{ .Data.server_token }}"
-                  {{ end }}
-                '';
-              };
-            };
-          };
-
-          "acme-ruby.hamho.me" = {
-            settings = {
-              vault.address = "https://vault.quartz.hamho.me";
-
-              auto_auth = {
-                method = [{
-                  type = "approle";
-
-                  config = {
-                    role_id_file_path = "/var/lib/vault-agent/acme/role-id";
-                    secret_id_file_path = "/var/lib/vault-agent/acme/secret-id";
-
-                    remove_secret_id_file_after_reading = false;
-                  };
-                }];
-              };
-            };
-
-            secrets.environment = {
-              force = true;
-              templates = {
-                dns = {
-                  text = ''
-                    {{ with secret "secret/dns/hamhome" }}
-                    DO_AUTH_TOKEN="{{ .Data.do_auth_token }}"
-                    {{ end }}
-                  '';
-                };
-              };
-            };
-          };
-        };
-      };
     };
 
     security = {
       doas = enabled;
       acme = {
-        enable = true;
+        enable = false;
       };
     };
 
@@ -157,26 +52,6 @@ in
         auto-snapshot = enabled;
       };
     };
-  };
-
-  nix = {
-    distributedBuilds = true;
-
-    buildMachines = [
-      (build-machine "zoisite" "aarch64-linux" 1)
-      (build-machine "wulfenite" "aarch64-linux" 1)
-      (build-machine "wavellite" "aarch64-linux" 1)
-      (build-machine "vivianite" "aarch64-linux" 1)
-      (build-machine "violane" "aarch64-linux" 2)
-      (build-machine "vesuvianite" "aarch64-linux" 2)
-    ];
-  };
-
-  services.hydra = {
-    enable = false;
-    hydraURL = "https://hydra.ruby.hamho.me";
-    notificationSender = "hydra@ruby.hamho.me";
-    useSubstitutes = true;
   };
 
   security.acme = {
@@ -198,26 +73,17 @@ in
 
     clientMaxBodySize = "256m";
 
-    virtualHosts =
-      let
-        shared-config = {
-          extra-config = {
-            forceSSL = true;
+    virtualHosts = let
+      shared-config = {
+        extra-config = {
+          forceSSL = true;
 
-            sslCertificate = "${config.security.acme.certs."ruby.hamho.me".directory}/fullchain.pem";
-            sslCertificateKey = "${config.security.acme.certs."ruby.hamho.me".directory}/key.pem";
-          };
+          sslCertificate = "${config.security.acme.certs."ruby.hamho.me".directory}/fullchain.pem";
+          sslCertificateKey = "${config.security.acme.certs."ruby.hamho.me".directory}/key.pem";
         };
-      in
-      {
-        "attic.ruby.hamho.me" =
-          network.create-proxy
-            ({
-              port = 8989;
-            }
-            // shared-config);
-        # "hydra.ruby.hamho.me" = network.create-proxy ({ port = 3000; } // shared-config);
       };
+    in {
+    };
   };
 
   system.stateVersion = "22.05";
