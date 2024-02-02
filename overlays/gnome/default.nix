1dbad84e06197624473d6bb1601c75d6bbdae23b
Jake Hamilton <jake.hamilton@hey.com>
2024-02-01 19:37:28 -0800

    fix: remove missing extension

--- overlays/gnome/default.nix
+++ overlays/gnome/default.nix
@@ -1,11 +1,9 @@
-{ channels, ... }:
-
-final: prev:
-
-let extensions = channels.unstable.gnomeExtensions;
-in
-{
-  gnomeExtensions = prev.gnomeExtensions // {
-    inherit (extensions) audio-output-switcher;
-  };
+{channels, ...}: final: prev: let
+  extensions = channels.unstable.gnomeExtensions;
+in {
+  gnomeExtensions =
+    prev.gnomeExtensions
+    // {
+      # inherit (extensions) audio-output-switcher;
+    };
 }
