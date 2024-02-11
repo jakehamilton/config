af7157f331d1c5f7daf74fa84a2b20e55b08ab0b
Jake Hamilton <jake.hamilton@hey.com>
2024-02-11 15:21:54 -0800

    fix: use updated prisma env vars

--- modules/nixos/cli-apps/prisma/default.nix
+++ modules/nixos/cli-apps/prisma/default.nix
@@ -1,26 +1,29 @@
-{ lib, pkgs, config, ... }:
-
-with lib;
-with lib.plusultra;
-let
-  cfg = config.plusultra.cli-apps.prisma;
-in
 {
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+with lib;
+with lib.plusultra; let
+  cfg = config.plusultra.cli-apps.prisma;
+in {
   options.plusultra.cli-apps.prisma = with types; {
     enable = mkBoolOpt false "Whether or not to install Prisma";
     pkgs = {
       npm = mkOpt package pkgs.nodePackages.prisma "The NPM package to install";
-      engines = mkOpt package pkgs.prisma-engines
+      engines =
+        mkOpt package pkgs.prisma-engines
         "The package to get prisma engines from";
     };
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [ cfg.pkgs.npm ];
+    environment.systemPackages = [cfg.pkgs.npm];
 
     plusultra.home.extraOptions = {
       programs.zsh.initExtra = ''
-        export PRISMA_MIGRATION_ENGINE_BINARY="${cfg.pkgs.engines}/bin/migration-engine"
+        export PRISMA_SCHEMA_ENGINE_BINARY="${cfg.pkgs.engines}/bin/schema-engine"
         export PRISMA_QUERY_ENGINE_BINARY="${cfg.pkgs.engines}/bin/query-engine"
         export PRISMA_QUERY_ENGINE_LIBRARY="${cfg.pkgs.engines}/lib/libquery_engine.node"
         export PRISMA_INTROSPECTION_ENGINE_BINARY="${cfg.pkgs.engines}/bin/introspection-engine"
