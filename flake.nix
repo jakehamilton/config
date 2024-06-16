f4751d60a484e348dfca264b7f40b98b7b08e29d
Jake Hamilton <jake.hamilton@hey.com>
2024-06-16 09:08:22 -0700

    chore: update aux website

--- flake.nix
+++ flake.nix
@@ -24,8 +24,8 @@
     nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
 
     # Snowfall Lib
-    # snowfall-lib.url = "github:snowfallorg/lib?ref=v3.0.0";
-    snowfall-lib.url = "path:/home/short/work/@snowfallorg/lib";
+    snowfall-lib.url = "github:snowfallorg/lib?ref=v3.0.0";
+    # snowfall-lib.url = "path:/home/short/work/@snowfallorg/lib";
     snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
 
     # Avalanche
