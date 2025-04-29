f68e4b325df9be3682caa9fd44cb26871df995d9
Jake Hamilton <jake.hamilton@hey.com>
2025-04-29 00:31:35 -0700

    wip: fht config

--- nilla/systems/nixos/modules/desktop/fht/default.nix
+++ nilla/systems/nixos/modules/desktop/fht/default.nix
@@ -1,6 +1,7 @@
 { lib, config, pkgs, project, ... }:
 let
   cfg = config.plusultra.desktop.fht;
+  term = config.plusultra.desktop.addons.term;
 
   package = project.inputs.fht-compositor.result.packages.${pkgs.system}.fht-compositor.override {
     inherit (pkgs)
@@ -20,14 +21,14 @@ let
       installShellFiles;
   };
 
-  toml = pkgs.formats.toml {};
+  toml = pkgs.formats.toml { };
 
   format = {
     inherit (toml) type;
     generate = name: value:
       let
         result = toml.generate name value;
-        checked = pkgs.runCommandNoCC "fht-compositor-checked-config" {} ''
+        checked = pkgs.runCommandNoCC "fht-compositor-checked-config" { } ''
           ${lib.getExe package} --config-path '${result}' check-configuration > ./message
 
           if [ $? -ne 0 ]; then
@@ -39,7 +40,7 @@ let
           touch $out
         '';
       in
-        builtins.seq checked result;
+      builtins.seq checked result;
   };
 
   settings = {
@@ -57,6 +58,178 @@ let
       MOZ_ENABLE_WAYLAND = "1";
       SDL_VIDEODRIVER = "wayland";
       SDL_BACKEND = "wayland";
+    };
+
+    general = {
+      layouts = [
+        # TODO: For large screens we want to use "centered-master" as the default.
+        # We should also provide both layouts and an
+        # easy way to switch between them at runtime
+        # in case an external monitor is attached.
+        "tile"
+      ];
+    };
+
+    input = {
+      keyboard = {
+        repeat-rate = 50;
+        repeat-delay = 200;
+      };
+
+      mouse = {
+        # TODO: This should be "linear" for desktops.
+        acceleration-profile = "adaptive";
+
+        scroll-method = "two-finger";
+        click-method = "click-finger";
+        natural-scrolling = true;
+
+        tap-to-click = true;
+        tap-button-map = "left-right-middle";
+
+        tap-and-drag = true;
+        drag-lock = true;
+      };
+    };
+
+    keybinds = {
+      Super-Return = {
+        action = "run-command";
+        arg = lib.getExe term.package;
+      };
+
+      Super-Control-Shift-q = {
+        action = "quit";
+      };
+
+      Super-Control-Shift-r = {
+        action = "reload-config";
+      };
+
+      Super-q = {
+        action = "close-focused-window";
+      };
+
+      Super-f = {
+        action = "fullscreen-focused-window";
+      };
+
+      Super-m = {
+        action = "maximize-focused-window";
+      };
+
+      Super-c = {
+        action = "center-floating-window";
+      };
+
+      Super-Space = {
+        action = "float-focused-window";
+      };
+
+      Super-1 = {
+        action = "focus-workspace";
+        arg = 0;
+      };
+
+      Super-Shift-1 = {
+        action = "send-focused-window-to-workspace";
+        arg = 0;
+      };
+
+      Super-2 = {
+        action = "focus-workspace";
+        arg = 1;
+      };
+
+      Super-Shift-2 = {
+        action = "send-focused-window-to-workspace";
+        arg = 1;
+      };
+
+      Super-3 = {
+        action = "focus-workspace";
+        arg = 2;
+      };
+
+      Super-Shift-3 = {
+        action = "send-focused-window-to-workspace";
+        arg = 2;
+      };
+
+      Super-4 = {
+        action = "focus-workspace";
+        arg = 3;
+      };
+
+      Super-Shift-4 = {
+        action = "send-focused-window-to-workspace";
+        arg = 3;
+      };
+
+      Super-5 = {
+        action = "focus-workspace";
+        arg = 4;
+      };
+
+      Super-Shift-5 = {
+        action = "send-focused-window-to-workspace";
+        arg = 4;
+      };
+
+      Super-6 = {
+        action = "focus-workspace";
+        arg = 5;
+      };
+
+      Super-Shift-6 = {
+        action = "send-focused-window-to-workspace";
+        arg = 5;
+      };
+
+      Super-7 = {
+        action = "focus-workspace";
+        arg = 6;
+      };
+
+      Super-Shift-7 = {
+        action = "send-focused-window-to-workspace";
+        arg = 6;
+      };
+
+      Super-8 = {
+        action = "focus-workspace";
+        arg = 7;
+      };
+
+      Super-Shift-8 = {
+        action = "send-focused-window-to-workspace";
+        arg = 7;
+      };
+
+      Super-9 = {
+        action = "focus-workspace";
+        arg = 8;
+      };
+
+      Super-Shift-9 = {
+        action = "send-focused-window-to-workspace";
+        arg = 8;
+      };
+
+      Super-0 = {
+        action = "focus-workspace";
+        arg = 9;
+      };
+
+      Super-Shift-0 = {
+        action = "send-focused-window-to-workspace";
+        arg = 9;
+      };
+    };
+
+    mousebinds = {
+      Super-Left = "swap-tile";
+      Super-Right = "resize-tile";
     };
   };
 in
@@ -88,7 +261,7 @@ in
     };
 
     plusultra.home = {
-      configFile.fht-compositor-config = lib.mkIf (cfg.settings != {}) {
+      configFile.fht-compositor-config = lib.mkIf (cfg.settings != { }) {
         target = "fht/compositor.toml";
         source = format.generate "fht-compositor-config" cfg.settings;
       };
